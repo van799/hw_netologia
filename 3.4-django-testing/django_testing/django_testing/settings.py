b9770f0098af7bf586e373c3d0a08582c723d303
van799 <lexxD@list.ru>
2022-10-06 21:40:00 +0300

    test

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -79,12 +79,11 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -124,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,112 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from students.models import Course, Student
+from model_bakery import baker
+
+
+# Arrange
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def factory_course():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def factory_student():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_one_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    responce = client.get('/api/v1/courses/3/')
+    data = responce.json()
+    # Assert
+    assert responce.status_code == 200
+    assert courses[2].name == data['name']
+
+
+@pytest.mark.django_db
+def test_get_list_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    responce = client.get('/api/v1/courses/')
+    data = responce.json()
+    # Assert
+    assert responce.status_code == 200
+    assert len(data) == len(courses)
+    for index, course in enumerate(data):
+        assert course['name'] == courses[index].name
+
+
+#
+@pytest.mark.django_db
+def test_get_filter_id_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    id_course = courses[0].id
+    responce = client.get(f'/api/v1/courses/?id={id_course}')
+    data = responce.json()
+    # Assert
+    assert responce.status_code == 200
+    assert id_course == data[0]['id']
+
+
+@pytest.mark.django_db
+def test_get_filter_name_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    name = courses[0].name
+    responce = client.get(f'/api/v1/courses/?name={name}')
+    data = responce.json()
+    # Assert
+    assert responce.status_code == 200
+    assert name == data[0]['name']
+
+
+@pytest.mark.django_db
+def test_get_create_course(client):
+    # Act
+    count = Course.objects.count()
+    data = {'name': 'TestCourse'}
+    responce = client.post('/api/v1/courses/', data=data)
+    # Assert
+    assert responce.status_code == 201
+    assert Course.objects.count() == count + 1
+
+
+@pytest.mark.django_db
+def test_get_patch_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    id_course = courses[0].id
+    data = {'name': 'patch_name'}
+    responce = client.patch(f'/api/v1/courses/{id_course}/', data=data)
+    # Assert
+    assert responce.status_code == 200
+    assert Course.objects.filter(id=id_course)[0].name == data['name']
+
+
+@pytest.mark.django_db
+def test_get_delete_course(client, factory_course):
+    # Act
+    courses = factory_course(_quantity=10)
+    id_course = courses[0].id
+    print(courses)
+    print(courses)
+    count = Course.objects.count()
+    responce = client.delete(f'/api/v1/courses/{id_course}/')
+    # Assert
+    assert responce.status_code == 204
+    assert count - 1 == Course.objects.count()
